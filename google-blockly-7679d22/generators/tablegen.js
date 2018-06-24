1c4385954e732a488dd7e037548a112bedcde515
pc0805a <pc0805a@gmail.com>
2018-06-24 22:23:23 +0800

    add generator

---
+++ google-blockly-7679d22/generators/tablegen.js
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Visual Blocks Language
+ *
+ * Copyright 2012 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Helper functions for generating JavaScript for blocks.
+ * @author fraser@google.com (Neil Fraser)
+ */
+'use strict';
+
+goog.provide('Blockly.TableGen');
+
+goog.require('Blockly.Generator');
+
+
+/**
+ * TableGen code generator.
+ * @type {!Blockly.Generator}
+ */
+Blockly.TableGen = new Blockly.Generator('TableGen');
+
+/**
+ * List of illegal variable names.
+ * This is not intended to be a security feature.  Blockly is 100% client-side,
+ * so bypassing this list is trivial.  This is intended to prevent users from
+ * accidentally clobbering a built-in object or function.
+ * @private
+ */
+Blockly.TableGen.addReservedWords(
+    // http://arduino.cc/en/Reference/HomePage
+  );
+
+ /**
+ * Order of operation ENUMs.
+ * https://developer.mozilla.org/en/JavaScript/Reference/Operators/Operator_Precedence
+ */
