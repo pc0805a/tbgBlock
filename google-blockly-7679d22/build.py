8d5ee19ef6c69e9cace6353b778f8d7dde816d9f
pc0805a <pc0805a@gmail.com>
2018-06-26 17:13:16 +0800

    restart

--- google-blockly-7679d22/build.py
+++ google-blockly-7679d22/build.py
@@ -206,11 +206,6 @@ class Gen_compressed(threading.Thread):
       self.gen_blocks()
 
     if ('generators' in self.bundles):
-      self.gen_generator("javascript")
-      self.gen_generator("python")
-      self.gen_generator("php")
-      self.gen_generator("lua")
-      self.gen_generator("dart")
       self.gen_generator("tablegen")
 
   def gen_core(self):
